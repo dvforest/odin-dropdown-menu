2f1d4d87c336eb0d22915548e1e8beafc8910c92
dvforest <dvforest@hotmail.com>
2025-08-08 19:58:45 -0400

    Setup prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+
+export default defineConfig([
+    {
+        files: ['**/*.js'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        rules: {},
+    },
+]);
